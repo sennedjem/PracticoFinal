aca5e31daa4a72bb4e4d65da115b5ddeb831d350
LosPibesBienMachos <sebastianariell@hotmail.com.ar>
2016-01-19 23:48:34 -0300

    borrados los xtend gen y agregado git ignore

--- Melomano-Dominio/src/main/generated-sources/xtend/melomano/dominio/hbmUtils/Runner.java
+++
@@ -1,8 +0,0 @@
-package melomano.dominio.hbmUtils;
-
-import melomano.dominio.hbmUtils.ServiceCommand;
-
-@SuppressWarnings("all")
-public interface Runner {
-  public abstract <T extends Object> T run(final ServiceCommand<T> s);
-}
